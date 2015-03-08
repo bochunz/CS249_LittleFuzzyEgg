72291ac3a9db580a574504467a819a2824f32f1a
husheyn <husheyn@sjtu.edu.cn>
2015-03-07 23:41:41 -0800

    Indexed updated

--- src/edu/ucla/cs249/littlefuzzyegg/tfidf/Indexed.java
+++ src/edu/ucla/cs249/littlefuzzyegg/tfidf/Indexed.java
@@ -3,7 +3,31 @@ package edu.ucla.cs249.littlefuzzyegg.tfidf;
 import java.util.*;
 
 public class Indexed<T> {
-	public Indexed(T obj) {
-		
+	private final T key;
+	private final BagOfTags bag;
+	
+	public Indexed(T key) {
+		this.key = key;
+		this.bag = new BagOfTags();
+	}
+	
+	public void setWeights(List<Tag> tags) {
+		bag.setWeights(tags);
+	}
+	
+	public void setWeights(List<Tag> tags, int weight) {
+		bag.setWeights(tags, weight);
+	}
+	
+	public T getKey() {
+		return key;
+	}
+	
+	public List<Tag> getTags() {
+		return bag.getTags();
+	}
+	
+	public int getWeight(Tag tag) {
+		return bag.getWeight(tag);
 	}
 }
