f89c2fd28433c017d8f2d8551689c988dbe87d4a
husheyn <husheyn@sjtu.edu.cn>
2015-03-07 22:55:56 -0800

    TfIdf API updated

--- src/edu/ucla/cs249/littlefuzzyegg/tfidf/TfIdf.java
+++ src/edu/ucla/cs249/littlefuzzyegg/tfidf/TfIdf.java
@@ -5,12 +5,14 @@ import java.util.*;
 import edu.ucla.cs249.littlefuzzyegg.data.*;
 
 public class TfIdf {
-
+	private final Map<Indexed<Product>, Integer> bags;
+	
 	public TfIdf(Map<Indexed<Product>, Integer> bags) {
-		
+		this.bags = new HashMap<Indexed<Product>, Integer>(bags);
 	}
 	
-	public double getTfIdf(BagOfTags query, Indexed<Product>) {
-		
+	public double getTfIdf(BagOfTags query, Indexed<Product> product) {
+		double ret = 0;
+		return ret;
 	}
 }
