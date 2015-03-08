ba57e03a996f2fe6a6bdea0f7c67451542f3afc2
Bochun Zhang <zbc0813@gmail.com>
2015-03-08 01:42:00 -0800

    LabelReader added

---
+++ src/edu/ucla/cs249/littlefuzzyegg/io/LabelReader.java
@@ -0,0 +1,11 @@
+package edu.ucla.cs249.littlefuzzyegg.io;
+
+import java.util.List;
+
+public class LabelReader {
+	public static List<String> ReadTrainOrders(String filename) {
+		
+		return null;
+	}
+
+}
--- src/edu/ucla/cs249/littlefuzzyegg/io/OrderReader.java
+++ src/edu/ucla/cs249/littlefuzzyegg/io/OrderReader.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
