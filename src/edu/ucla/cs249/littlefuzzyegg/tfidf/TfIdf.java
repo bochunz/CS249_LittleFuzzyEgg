57776c6af66e722e0b323bc96320b7d367af77ce
husheyn <husheyn@sjtu.edu.cn>
2015-03-07 22:04:45 -0800

    modified:   src/edu/ucla/cs249/littlefuzzyegg/tfidf/TfIdf.java 	src/edu/ucla/cs249/littlefuzzyegg/tfidf/BagOfTags.java 	src/edu/ucla/cs249/littlefuzzyegg/tfidf/Indexed.java 	src/edu/ucla/cs249/littlefuzzyegg/tfidf/Tag.java

--- src/edu/ucla/cs249/littlefuzzyegg/tfidf/TfIdf.java
+++ src/edu/ucla/cs249/littlefuzzyegg/tfidf/TfIdf.java
@@ -1,5 +1,16 @@
 package edu.ucla.cs249.littlefuzzyegg.tfidf;
 
+import java.util.*;
+
+import edu.ucla.cs249.littlefuzzyegg.data.*;
+
 public class TfIdf {
 
+	public TfIdf(Map<Indexed<Product>, Integer> bags) {
+		
+	}
+	
+	public double getTfIdf(BagOfTags query, Indexed<Product>) {
+		
+	}
 }
