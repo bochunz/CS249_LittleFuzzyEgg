676b8bd68c17570317945fb763c51a1db6c4a7c0
sushaoxiang911 <sushaoxiang911@s-149-142-31-167.resnet.ucla.edu>
2015-03-08 22:20:08 -0700

    hard code frequent words

--- src/edu/ucla/cs249/littlefuzzyegg/split/Dictionary.java
+++ src/edu/ucla/cs249/littlefuzzyegg/split/Dictionary.java
@@ -12,6 +12,7 @@ import edu.ucla.cs249.littlefuzzyegg.tfidf.Tag;
 public class Dictionary {
 	private static int ELIMINATE_LENGTH = 2;
 	private Set<String> words;
+	private Set<String> importantWords;
 	private static Dictionary instance = null;
 	
 	private Dictionary() {
@@ -145,6 +146,105 @@ public class Dictionary {
 	            "deus",
 	            "xmen"
 	    ));
+		importantWords = new HashSet<String> (Arrays.asList(
+				"fifa",
+				"ncis",
+				"wolfenstein",
+				"otomedius",
+				"borderlands",
+				"shaddai",
+				"tropico",
+				"lucha",
+				"fable",
+				"yoostar",
+				"brink",
+				"sixstring",
+				"divinity",
+				"zumba",
+				"crysis",
+				"libre",
+				"sonic",
+				"rapala",
+				"uncaged",
+				"kengo",
+				"megamind",
+				"kinectimals",
+				"fallout",
+				"singularity",
+				"halo",
+				"dreamworks",
+				"jasf",
+				"nfl",
+				"nickelodeon",
+				"hogan",
+				"persia",
+				"vanquish",
+				"battlefield",
+				"spongebob",
+				"creed",
+				"wwe",
+				"bulletstorm",
+				"xmen",
+				"mysims",
+				"nba",
+				"rez",
+				"homefront",
+				"jurassic",
+				"disney",
+				"grease",
+				"transformers",
+				"udraw",
+				"ufc",
+				"guitar",
+				"capcom",
+				"gauntlet",
+				"mma",
+				"redemption",
+				"ncaa",
+				"rocksmith",
+				"thor",
+				"mindjack",
+				"shred",
+				"overstrike",
+				"assassin",
+				"katamari",
+				"cabela",
+				"pga",
+				"nhl",
+				"catherine",
+				"goldeneye",
+				"adrenalin",
+				"madagascar",
+				"lego",
+				"facebreaker",
+				"blackwater",
+				"madden",
+				"prototype",
+				"darkstar",
+				"batman",
+				"forza",
+				"nukem",
+				"brunswick",
+				"hasbro",
+				"bodycount",
+				"castlevania",
+				"nascar",
+				"mayhem",
+				"armageddon",
+				"ssx",
+				"csi",
+				"popcap",
+				"gunstringer",
+				"fracture",
+				"deus",
+				"tekken",
+				"darksiders",
+				"arcania",
+				"agarest",
+				"konami",
+				"n3",
+				"marvel"));
+		
 	}
 	
 	public static Dictionary getInstance() {
@@ -161,8 +261,15 @@ public class Dictionary {
 		}
 	}
 	
-	public boolean isImportant(Tag tag) {
-		return false;
+	public boolean isImportant(Tag tag) {		
+		if (tag.getType() == Tag.Type.ACRONYM)
+			return false;
+		else if (tag.getType() == Tag.Type.ALSO) 
+			return false;
+		else if (tag.getType() == Tag.Type.DATE)
+			return false;
+		else 
+			return importantWords.contains(tag.getValue()); 
 	}
 	
 	public List<String> getList() {
